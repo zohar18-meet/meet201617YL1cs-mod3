384b355f8cd3ecf135c425bc2189b454a6352fb8
golfitgolfit <tedg@meet.mit.edu>
2016-11-16 15:54:51 -0500

    Added a missing comma

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -1,10 +1,10 @@
 #This script performs some simple tests on the UserAccount class.
 import UserAccount
 
-#Two things are missing from the line below - fill them in
-my_user=UserAcount(  ,  )
+#Three things are missing from the line below - fill them in
+my_user=UserAcount(  ,  ,  )
 
-#Call the print_password method (function) - it takes one input - a guess for the password.
+#Call the print_secret method (function) - it takes one input - a guess for the password.
 
 #Use the wrong password as input here
 my_user.
