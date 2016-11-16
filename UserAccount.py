8bcf8b22bc0f88a98514bc9c1f2b17eff5a56854
golfitgolfit <tedg@meet.mit.edu>
2016-11-16 09:29:27 -0500

    Changed name of method to be more transparent.

--- UserAccount.py
+++ UserAccount.py
@@ -1,1 +1,25 @@
-/media/golfit/share/MEET/YearLong/YearLong2016/YL1/Module3/UserAccount.py
+class UserAccount :
+    """
+    A class that models a user account for mega-social-media website, GuessMySecret
+    """
+
+    def __init__(username, password, secret):
+        """
+        Initialize the UserAccount object:
+        :param username: user name (string)
+        :param password: password to gain access to account (string)
+        :param secret: user's secret, accessible only with passworld (string)
+        """
+        self.username=username
+        #Complete the __init__ method
+
+    def print_password(self,password_attempt):
+        """
+        Checks user input, password_attempt - if this matches the user's
+        password, then the user's secret is printed to the screen.
+
+        :param password_attempt: guess for the password (string)
+        """
+        #Complete this method - check the input variable, password_attempt,
+        #against the password for this user (the value of the instance variable, password).
+        #If, and only if, there's a match, then print the user's secret to the screen.
