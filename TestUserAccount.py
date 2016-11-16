e4aeebb00e403eb8b40b122bb15d9eaa060552c3
golfitgolfit <tedg@meet.mit.edu>
2016-11-16 15:39:16 -0500

    Changed name of method in UserAccount to clarify; added TestUserAccount

---
+++ TestUserAccount.py
@@ -0,0 +1,12 @@
+#This script performs some simple tests on the UserAccount class.
+
+#Two things are missing from the line below - fill them in
+my_user=UserAcount(  ,  )
+
+#Call the print_password method (function) - it takes one input - a guess for the password.
+
+#Use the wrong password as input here
+my_user.
+
+#Use the right password here
+my_user.
